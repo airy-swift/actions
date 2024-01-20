024a8d9239a7581b761edf8cbc7e5ff99efbf818
airy-swift <thanks.xa@gmail.com>
2024-01-20 18:29:49 +0900

    fix

--- shell_script/version_auto_increment.sh
+++ shell_script/version_auto_increment.sh
@@ -9,5 +9,5 @@ perl -i -pe 's/^(version:\s+\d+\.\d+\.\d+\+)(\d+)$/$1.($2+1)/e' pubspec.yaml
 version=`grep 'version: ' pubspec.yaml | sed 's/version: //'`
 git commit -m "Bump version to $version" pubspec.yaml
 git tag $version
-git push
+git push origin head
 git push origin $version
